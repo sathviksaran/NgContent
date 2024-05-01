754fa27eb8a93a3db4143d12e08c9a6a6e561828
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2024-05-01 13:43:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from "./product/product.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ProductComponent]
 })
 export class AppComponent {
   title = 'NgContent';
